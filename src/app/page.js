f79dcce5dfb5bab0f851e6ac4c8143c0158533db
unknown <t.hasan.prince@gmail.com>
2023-11-26 15:48:35 +0600

    [Refactored]- Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,9 @@
-import Link from "next/link";
+import Header from "./components/layout/Header";
 
 export default function Home() {
   return (
     <>
-      <header className="flex items-center justify-between">
-        <Link className="text-primary  font-bold text-2xl" href="">
-          Pure Pump
-        </Link>
-        <nav className="flex items-center gap-6 text-gray-600 font-semibold">
-          <Link href={""}>Home</Link>
-          <Link href={""}>Supplements</Link>
-          <Link href={""}>Accesories</Link>
-          <Link href={""}>About</Link>
-          <Link href={""}>Contact</Link>
-          <Link href={""} className="bg-primary text-white px-8 py-2 rounded-md">Login</Link>
-        </nav>
-      </header>
+      <Header></Header>
     </>
   );
 }
