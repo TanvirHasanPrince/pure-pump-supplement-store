378acc30ae15446dee8647b44d30afd9f1226bac
unknown <t.hasan.prince@gmail.com>
2023-12-04 00:08:00 +0600

    [Done]- Profile Update General Full

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,12 +1,16 @@
 import { model, models, Schema } from "mongoose";
 
-
 const UserSchema = new Schema(
   {
     name: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String },
     image: { type: String },
+    phone: { type: String },
+    houseAddress: { type: String },
+    city: { type: String },
+    postCode: { type: String },
+    country: { type: String },
   },
   {
     timestamps: true,
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -11,14 +11,28 @@ const ProfilePage = () => {
   const session = useSession();
   const [userName, setUserName] = useState("");
   const [image, setImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [houseAddress, setHouseAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
   const [isSaving, setIsSaving] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
   const { status } = session;
 
   useEffect(() => {
-    if (status === "authenticated" && session?.data?.user?.name) {
+    if (status === "authenticated") {
       setUserName(session.data.user.name);
       setImage(session?.data?.user?.image);
+      fetch("/api/profile").then((response) => {
+        response.json().then((data) => {
+          setPhone(data?.phone);
+          setHouseAddress(data?.houseAddress);
+          setCity(data?.city);
+          setPostalCode(data?.postCode);
+          setCountry(data?.country);
+        });
+      });
     }
   }, [session, status]);
 
@@ -36,7 +50,15 @@ const ProfilePage = () => {
     const response = await fetch("/api/profile", {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: userName, image }),
+      body: JSON.stringify({
+        name: userName,
+        image,
+        phone,
+        houseAddress,
+        city,
+        postCode,
+        country,
+      }),
     });
     setIsSaving(false);
     if (response.ok) {
@@ -74,7 +96,7 @@ const ProfilePage = () => {
       {isSaving && <InfoText>Saving...</InfoText>}
       {isUploading && <InfoText>Uploading...</InfoText>}
       <div className="max-w-lg mx-auto">
-        <div className="flex gap-4 items-center">
+        <div className="flex gap-4">
           <div>
             <div className="relative p-2 rounded-lg max-w-[120px]">
               {image && (
@@ -97,17 +119,61 @@ const ProfilePage = () => {
           </div>
 
           <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <label> Name</label>
             <input
               type="text"
               placeholder="Your Name"
               value={userName}
               onChange={(ev) => setUserName(ev.target.value)}
             />
+
+            <label>Email</label>
             <input
               disabled
               type="email"
               placeholder="Your Name"
               value={session?.data?.user?.email}
+            />
+            <label> Phone number</label>
+            <input
+              type="tel"
+              placeholder="Phone number"
+              value={phone}
+              onChange={(ev) => setPhone(ev.target.value)}
+            />
+            <label> House Address</label>
+            <input
+              type="text"
+              placeholder="House address"
+              value={houseAddress}
+              onChange={(ev) => setHouseAddress(ev.target.value)}
+            />
+            <div className="flex gap-2">
+              <div>
+                <label> City</label>
+                <input
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  onChange={(ev) => setCity(ev.target.value)}
+                />
+              </div>
+              <div>
+                <label> Post Code</label>
+                <input
+                  type="text"
+                  placeholder="Postal code"
+                  value={postCode}
+                  onChange={(ev) => setPostalCode(ev.target.value)}
+                />
+              </div>
+            </div>
+            <label> Country</label>
+            <input
+              type="text"
+              placeholder="Country"
+              value={country}
+              onChange={(ev) => setCountry(ev.target.value)}
             />
             <button type="submit">Save</button>
           </form>
@@ -118,8 +184,6 @@ const ProfilePage = () => {
 };
 
 export default ProfilePage;
-
-
 
 // Another way to use the react hot toast---- with promise
 // const savingPromise = new Promise(asynch(resolve, reject)=> {
