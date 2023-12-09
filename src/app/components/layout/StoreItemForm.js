54e49f4829ec30a5d5ee3934347ed6324beda52d
unknown <t.hasan.prince@gmail.com>
2023-12-10 01:18:52 +0600

    [Done]- Add Sizes to Menu Items

--- src/app/components/layout/StoreItemForm.js
+++ src/app/components/layout/StoreItemForm.js
@@ -6,6 +6,27 @@ const StoreItemForm = ({ onSubmit, storeItem }) => {
   const [name, setName] = useState(storeItem?.name || "");
   const [description, setDescription] = useState(storeItem?.description || "");
   const [basePrice, setIBasePrice] = useState(storeItem?.basePrice || "");
+  const [sizes, setSizes] = useState([]);
+
+  function addSize() {
+    setSizes((oldSizes) => {
+      return [...oldSizes, { name: "", price: 0 }];
+    });
+  }
+
+  function editSize(ev, index, prop) {
+    const newValue = ev.target.value;
+    setSizes((prevSizes) => {
+      const newSizes = [...prevSizes];
+      newSizes[index][prop] = newValue;
+      return newSizes;
+    });
+  }
+
+  function removeSize(index) {
+    setSizes(prev => prev.filter((v,i)=> i !== index))
+  }
+
   return (
     <form
       onSubmit={(ev) =>
@@ -14,7 +35,6 @@ const StoreItemForm = ({ onSubmit, storeItem }) => {
           name,
           description,
           basePrice,
-    
         })
       }
       className="mt-8 max-w-lg mx-auto"
@@ -45,6 +65,49 @@ const StoreItemForm = ({ onSubmit, storeItem }) => {
             value={basePrice}
             onChange={(ev) => setIBasePrice(ev.target.value)}
           />
+          <div className="bg-gray-200 p-2 rounded-md mb-2">
+            <label>Sizes</label>
+            {sizes.length > 0 &&
+              sizes.map((size, index) => (
+                <div className="flex items-end gap-2" key={index}>
+                  <div>
+                    <labe>Size Name</labe>
+                    <input
+                      type="text"
+                      placeholder="Size Name"
+                      value={size?.name}
+                      onChange={(ev) => editSize(ev, index, "name")}
+                    />
+                  </div>
+                  <div>
+                    <labe>Extra Price</labe>
+                    <input
+                      type="text"
+                      placeholder="Extra Price"
+                      value={size?.price}
+                      onChange={(ev) => editSize(ev, index, "price")}
+                    />
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      className="bg-white p-2 mb-2  font-bold text-black w-full"
+                      onClick={() => removeSize(index)}
+                    >
+                      X
+                    </button>
+                  </div>
+                </div>
+              ))}
+
+            <button
+              type="button"
+              onClick={addSize}
+              className="bg-white p-2 rounded-lg font-bold text-black w-full"
+            >
+              Add Size (like 2lb or 5lb){" "}
+            </button>
+          </div>
           <button type="submit">Save</button>
         </div>
       </div>
