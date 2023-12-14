b90041fb3739740359c2b8e171abb075c01c3f96
unknown <t.hasan.prince@gmail.com>
2023-12-14 15:34:59 +0600

    [Done]- saveCartProductsToLocalStorage

--- src/app/components/appContext.js
+++ src/app/components/appContext.js
@@ -1,16 +1,31 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({});
 
 const AppProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([]);
+  const localStorage =
+    typeof window !== "undefined" ? window.localStorage : null;
+
+  function saveCartProductsToLocalStorage(cartProducts) {
+    if (localStorage) {
+      localStorage.setItem("cart", JSON.stringify(cartProducts));
+    }
+  }
+
+  useEffect(() => {
+    if (localStorage && localStorage.getItem("cart")) {
+      setCartProducts(JSON.parse(localStorage.getItem("cart")));
+    }
+  }, []);
 
   function addToCart(productId, size = null, extras = []) {
     const cartProducts = { ...productId, size, extras };
     setCartProducts((previousProducts) => {
       const newProducts = [...previousProducts, cartProducts];
+      saveCartProductsToLocalStorage(newProducts);
       return newProducts;
     });
   }
