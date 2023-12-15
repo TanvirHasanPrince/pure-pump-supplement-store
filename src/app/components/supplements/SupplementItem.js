b737f42bd2339e0b1d8eb68eca11e68165beaf93
unknown <t.hasan.prince@gmail.com>
2023-12-15 16:47:31 +0600

    [Ongoing]- SupplementItem

--- src/app/components/supplements/SupplementItem.js
+++ src/app/components/supplements/SupplementItem.js
@@ -3,6 +3,7 @@ import React, { useContext, useState } from "react";
 import { CartContext } from "../appContext";
 import toast from "react-hot-toast";
 import SupplementItemTile from "../supplements/SupplementItemTile";
+import Image from "next/image";
 
 const SupplementItem = (supplementItem) => {
   const { image, name, description, basePrice, sizes, category, flavour } =
@@ -24,7 +25,60 @@ const SupplementItem = (supplementItem) => {
     <>
       {showPopup && (
         <div className=" fixed inset-0 bg-black/80 flex items-center justify-center">
-          <div className="bg-white p-4 rounded-lg">Test</div>
+          <div className="my-8 bg-white p-2 rounded-lg max-w-md">
+            <div
+              className="overflow-y-scroll p-2"
+              style={{ maxHeight: "calc(100vh - 100px)" }}
+            >
+              <Image
+                src={image}
+                width={200}
+                height={200}
+                className="mx-auto"
+                alt="Supplement Image"
+              ></Image>
+              <h1 className="text-lg font-bold text-center mb-2">{name}</h1>
+              <p className="text-center text-gray-500 text-sm mb-2">
+                {description}
+              </p>
+
+              {sizes?.length > 0 && (
+                <div className="rounded-md p-2">
+                  <h3 className="text-center text-gray-500 mb-2">Pick Size</h3>
+                  {sizes.map((size, i) => (
+                    <label
+                      className="flex items-center gap-2 p-4 rounded-md mb-1 border"
+                      key={i}
+                    >
+                      <input type="radio" name={size.name} /> {size.name} BDT{" "}
+                      {basePrice + size.price}
+                    </label>
+                  ))}
+                </div>
+              )}
+
+              {flavour?.length > 0 && (
+                <div className="rounded-md p-2">
+                  <h3 className="text-center text-gray-500 mb-2">
+                    Pick Flavour
+                  </h3>
+                  {flavour.map((flavor, i) => (
+                    <label
+                      className="flex items-center gap-2 p-4 rounded-md mb-1 border"
+                      key={i}
+                    >
+                      <input type="radio" name={flavor.name} /> {flavor.name}{" "}
+                      BDT {flavor.price}
+                    </label>
+                  ))}
+                </div>
+              )}
+              <button className="primary" type="button">
+                {" "}
+                Add to cart selected price{" "}
+              </button>
+            </div>
+          </div>
         </div>
       )}
       <SupplementItemTile
