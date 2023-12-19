b756c9f1f0acb39335bfcda406da5827f26c6bc9
unknown <t.hasan.prince@gmail.com>
2023-12-20 03:18:35 +0600

    [Done]- Succeefully fetching order in the orders page

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -1,11 +1,30 @@
-
+"use client";
+import { useEffect, useState } from "react";
+import SectionHeaders from "../components/layout/SectionHeaders";
 
 const OrdersPage = () => {
- return (
-  <div>
-   
-  </div>
- );
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/orders").then((res) => {
+      res.json().then(orders => {
+       setOrders(orders)
+      });
+    });
+  }, []);
+
+  return (
+    <section className="mt-8 max-w-2xl mx-auto">
+      <div className="text-center">
+        <SectionHeaders mainHeader={"Orders"}></SectionHeaders>
+      </div>
+      {
+       orders?.length > 0 && orders.map((order)=> (
+        <div key={order._id}>{order.createdAt}</div>
+       ))
+      }
+    </section>
+  );
 };
 
 export default OrdersPage;
