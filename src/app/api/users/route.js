b4953075860670ccdb00e91f962b20c86a4ffd78
unknown <t.hasan.prince@gmail.com>
2023-12-23 15:23:18 +0600

    [Bug Fix]- User Route- Admin

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -6,7 +6,7 @@ export async function GET() {
   mongoose.connect(process.env.MONGO_URL);
   if (await isAdmin()) {
     const users = await User.find();
-
+    console.log(users);
     return Response.json(users);
   } else {
     return Response.json([]);
