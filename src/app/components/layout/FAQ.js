7ec3d1967fe8257117a95319a3bffa71fc64494a
unknown <t.hasan.prince@gmail.com>
2023-12-25 12:31:16 +0600

    [Done]- FAQ and Footer Fixed

--- src/app/components/layout/FAQ.js
+++ src/app/components/layout/FAQ.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 const Accordion = () => {
   return (
-    <section className="relative z-20 overflow-hidden bg-white pt-20 dark:bg-dark my-2 ">
+    <section className="relative z-20 overflow-hidden bg-white pt-20 dark:bg-dark ">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
